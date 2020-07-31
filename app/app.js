12ade760d7201d7377d630b55091a07c885f1952
Xavier Salazar <x.salazar1729@gmail.com>
2020-07-30 22:32:04 -0700

    Update Express app for setting Redis data with query params Add gifs endpoint for making external API requests

--- app/app.js
+++ app/app.js
@@ -1,15 +1,23 @@
 const express = require('express')
+const axios = require('axios')
 const { keys, set } = require('./redis')
 const app = express()
+app.set('view engine','ejs')
 
 app.get('/', async (req, res) => {
     res.json(await keys())
 })
 
-app.get('/:key/:value', async (req, res) => {
-    const { key, value } = req.params
+app.get('/redis', async (req, res) => {
+    const { key, value } = req.query
     await set(key, value)
     res.send(`Set ${key} to ${value}`)
+})
+
+app.get('/gif/:tag', async (req, res) => {
+    const { tag } = req.params
+    const result = await axios.get(`http://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}&tag=${tag}`)
+    res.render('gifs', {url: result.data.data.embed_url })
 })
 
 module.exports = app;
